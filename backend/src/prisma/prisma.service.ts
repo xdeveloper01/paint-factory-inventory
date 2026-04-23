e3ffb322cd9dd0bbc7795582fc652cad99cc8092
xdeveloper01 <chadelammar90@gmail.com>
2026-04-23 15:05:30 +0100

    Fix tsconfig errors and simplify PrismaService constructor

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -4,13 +4,7 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    super({
-      datasources: {
-        db: {
-          url: process.env.DATABASE_URL,
-        },
-      },
-    });
+    super();
   }
 
   async onModuleInit() {
